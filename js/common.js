03663fb191d7b8f6a9503a55d10fbd3a5b07f34c
Alex <rumbaaaa01@gmail.com>
2016-11-13 22:57:55 +0200

    Add js for ToDo list.

--- js/common.js
+++ js/common.js
@@ -2,3 +2,43 @@ $(window).load(function() {
 	$(".loader_inner").fadeOut();
 	$(".loader").delay(400).fadeOut("slow");
 });
+
+$(document).ready(function(){
+addListenerToAddTask();
+});
+function addListenerToAddTask(){
+	$(".add-task").click(function(){
+		var valueTask = $(".input-task").val();
+		if(!valueTask.trim()){
+			alert("Please, enter your text!")
+			return false
+		}
+		var containerTodo = "<div class='todo-container'> <strong>"+ valueTask +"</strong> <button class='edit-task'>Edit</button><button class='delete-task'>Delete</button></div>";
+		$(".todos-container").append(containerTodo);
+		$(".input-task").val("");
+		addListenerToDeleteTask();
+		addListenerToEditTask();
+	});
+}
+function addListenerToDeleteTask(){
+$(".delete-task").click(function(){
+	$(this).parents(".todo-container").remove();
+});
+}
+function addListenerToEditTask(){
+$(".edit-task").click(function(){
+var valueEditTask = $(this).siblings("strong");
+$("#myModal").modal("show");
+var inputForEdit = $(".input-task-edit");
+inputForEdit.val(valueEditTask.text());
+$(".save-changes").unbind("click");
+$(".save-changes").click(function(){
+	if(!inputForEdit.val().trim()){
+		alert("Please, enter your text!")
+		return false
+	}
+	valueEditTask.text(inputForEdit.val());
+});
+
+});
+}
